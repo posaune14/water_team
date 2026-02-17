b298eb3e998f4549589b2a318aa0b80e291536a8
nbhavsar133 <nprbhvsr@gmail.com>
2026-02-17 11:56:00 -0500

    added diagonal movement

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -39,52 +39,93 @@ public class Movement : MonoBehaviour
     {
          float moveX = Input.GetAxisRaw("Horizontal");
          float moveY = Input.GetAxisRaw("Vertical");
+
          Debug.Log(moveX);
-         if (moveX>0)
+         Debug.Log(moveY);
+
+         m_Animator.SetBool("right_bool",false);
+         m_Animator.SetBool("left_bool",false);
+         m_Animator.SetBool("up_bool",false);
+         m_Animator.SetBool("down_bool",false);
+         m_Animator.SetBool("Rup_bool",false);
+         m_Animator.SetBool("rd_bool",false);
+         m_Animator.SetBool("Lup_bool",false);
+         m_Animator.SetBool("Ld_bool",false);
+         
+         if (moveX>0 && moveY>0)
          {
-
-            m_Animator.SetBool("right_bool",true);
+            m_Animator.SetBool("Rup_bool",true);
             sprite_renderer.flipX = false;
-            
-            
-         }else{
-
-            m_Animator.SetBool("right_bool",false);
-
          }
 
-         if (moveX<0)
+         else if (moveX>0 && moveY<0)
+         {
+            m_Animator.SetBool("rd_bool",true);
+            sprite_renderer.flipX = false;
+         }
+
+         else if (moveX<0 && moveY>0)
+         {
+            m_Animator.SetBool("Lup_bool",true);
+            sprite_renderer.flipX = true;
+         }
+
+         else if (moveX<0 && moveY<0)
+         {
+            m_Animator.SetBool("Ld_bool",true);
+            sprite_renderer.flipX = true;
+         }
+         else if(moveX>0)
+         {
+            m_Animator.SetBool("right_bool",true);
+            sprite_renderer.flipX = false;
+         }
+         // if (moveX>0)
+         // {
+
+         //    m_Animator.SetBool("right_bool",true);
+         //    sprite_renderer.flipX = false;
+            
+            
+         // }else{
+
+         //    m_Animator.SetBool("right_bool",false);
+
+         // }
+
+         else if (moveX<0)
          {
 
             m_Animator.SetBool("left_bool", true);
             sprite_renderer.flipX = true;
-    
             
-            
-         }else{
-            m_Animator.SetBool("left_bool", false);
          }
+         // else{
+         //    m_Animator.SetBool("left_bool", false);
+         // }
 
          
-         Debug.Log(moveY);
-         if (moveY>0)
+         
+         else if (moveY>0)
 
          {
 
             m_Animator.SetBool("up_bool", true);
 
-         }else{
-            m_Animator.SetBool("up_bool",false);
          }
+         // else{
+         //    m_Animator.SetBool("up_bool",false);
+         // }
 
-         if (moveY<0)
+         else if (moveY<0)
          {
 
             m_Animator.SetBool("down_bool",true);
 
-         }else{
-            m_Animator.SetBool("down_bool",false);
          }
+         // else{
+         //    m_Animator.SetBool("down_bool",false);
+         // }
 
          moveDirection = new Vector2(moveX, moveY).normalized;   
     }
