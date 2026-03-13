1c46cd31678a6d96a9f5e2c154c25a9ed7750bb5
Mahil Jayaprakash <asamingogee1@gmail.com>
2026-03-13 17:16:44 -0400

    Fix slice movement

--- Assets/Scripts/Slash.cs
+++ Assets/Scripts/Slash.cs
@@ -7,10 +7,24 @@ public class Slash : MonoBehaviour
     public GameObject square;
     //public Transform player;
     public Transform target;
+    public Color current_color;
+    private SpriteRenderer spriterenderer;
+
     void Start()
     {
-        target = GameObject.FindWithTag("Player").transform;
-        square.SetActive(false);
+        spriterenderer = GetComponent<SpriteRenderer>();
+        Debug.Log(spriterenderer);
+          if (spriterenderer != null)
+        {
+            //Debug.Log(spriterenderer.color);
+            
+            current_color = spriterenderer.color;
+            current_color.a = 0;
+            spriterenderer.color = current_color;
+            target = GameObject.FindWithTag("Player").transform;
+
+        }
+        
     }
 
     // Update is called once per frame
@@ -18,18 +32,23 @@ public class Slash : MonoBehaviour
     {
         if(Mouse.current.leftButton.wasPressedThisFrame)
         {
-
+            current_color.a = 1;
+            spriterenderer.color = current_color;
+            Debug.Log(current_color.a);
             transform.position = target.position;
-            square.SetActive(true);
-            Debug.Log("left clicked." + target.position);
+            //square.SetActive(true);
+            //Debug.Log("left clicked." + target.position);
             Invoke("Disappear", 0.45f);
+            Debug.Log(current_color.a);
             
 
         }
     }
 
     void Disappear(){
-        square.SetActive(false);
+        current_color.a = 0f;
+        spriterenderer.color = current_color;
+        //square.SetActive(false);
     }
     
 }
