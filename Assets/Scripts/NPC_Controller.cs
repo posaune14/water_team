fa251457e028ab9541f023060ffc020408fbb519
posaune14 <michaelyoutz3@gmail.com>
2026-03-17 16:27:11 -0400

    NPC_Controller file started

---
+++ Assets/Scripts/NPC_Controller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPC_Controller : MonoBehaviour
+{
+    /*public int maxHealth = 100;
+    public int curHealth;
+    public int panicMultiplier = 1;
+    
+    public Node currentNode;
+    public List<Node> path;
+
+    public PlayerController player;
+    public float speed = 3;
+
+    public enum StateMachine
+    {
+        Patrol,
+        Engage,
+        Evade
+    }
+
+    public StateMachine currentState;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void Patrol()
+    {
+        
+    }*/
+}
